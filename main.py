6f2fe5c66921b6c67dc108b479640a8ca7357590
pafnootiy <ksander-86@mail.ru>
2022-06-15 22:40:59 +0300

    wrapped into ifmain

--- main.py
+++ main.py
@@ -6,49 +6,62 @@ from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-load_dotenv()
 
-path_to_file = os.getenv("PATH_TO_FILE")
+def get_year_of_founder():
+    date_now = datetime.now()
+    founder_year = (date_now.year - 1920)
 
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-date_now = datetime.now()
-founder_year = (date_now.year - 1920)
-
-if founder_year == 100:
-    text_year = "лет"
-elif founder_year == 101:
-    text_year = "год"
-elif 101 < founder_year <= 104:
-    text_year = "годa"
-elif founder_year > 104:
-    text_year = "лет"
+    if founder_year == 100:
+        text_year = "лет"
+        return founder_year, text_year
+    elif founder_year == 101:
+        text_year = "год"
+        return founder_year, text_year
+    elif 101 < founder_year <= 104:
+        text_year = "годa"
+        return founder_year, text_year
+    elif founder_year > 104:
+        text_year = "лет"
+        return founder_year, text_year
 
 
-wine_table = pd.read_excel(path_to_file, sheet_name='Лист1',
-                           na_values='nan', keep_default_na=False)
+def give_data_for_site_fill(path_to_file):
+    wine_table = pd.read_excel(path_to_file, sheet_name='Лист1',
+                               na_values='nan', keep_default_na=False)
+    wines = wine_table.to_dict(orient="records")
+    final_form = defaultdict(list)
 
-wines = wine_table.to_dict(orient="records")
-final_form_of_wines = defaultdict(list)
+    for wine in wines:
+        category = wine.get("Категория")
+        final_form[category].append(wine)
+    return final_form
 
-for wine in wines:
-    category = wine.get("Категория")
-    final_form_of_wines[category].append(wine)
 
-rendered_page = template.render(
-    founder_date=f"Уже {founder_year} {text_year} с вами",
-    categories=final_form_of_wines.keys(),
-    wines=final_form_of_wines
-)
+def main():
+    load_dotenv()
+    path_to_file = os.getenv("PATH_TO_FILE")
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    founder_year, text_year = get_year_of_founder()
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
+    final_form_of_wines = give_data_for_site_fill(path_to_file)
+
+    rendered_page = template.render(
+        founder_date=f"Уже {founder_year} {text_year} с вами",
+        categories=final_form_of_wines.keys(),
+        wines=final_form_of_wines
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
